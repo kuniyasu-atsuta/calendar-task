8358806f0517c0239d3ce27b3b78da2f8f5efdbb
kuniyasu-atsuta <atsuta05@gmail.com>
2019-06-20 19:08:58 +0000

    パスワード記憶機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@
 #  email           :string(255)
 #  name            :string(255)
 #  password_digest :string(255)
+#  remember_digest :string(255)
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #
@@ -15,6 +16,8 @@
 #
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -23,4 +26,31 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # 文字列のハッシュ化
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムな文字列の生成（トークの生成）
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 自身のトークンに新しいトークンを代入しデータベースを更新
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticate?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
